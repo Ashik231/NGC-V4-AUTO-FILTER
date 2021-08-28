735ec27cb507fdb6b4e805f173ece4e5b76efd28
Ashik231 <85797232+Ashik231@users.noreply.github.com>
2021-08-28 12:44:34 +0530

    Create script.py

---
+++ script.py
@@ -0,0 +1,30 @@
+class script(object):
+
+
+    START_MSG = """ <b>Hi {}
+
+You can call this as an Auto Filter Bot if you like :D
+
+This bot is exclusively made for @dcompanymoviess
+
+You can see me in @dcompanymoviess <i>help</i></b>"""
+
+
+    HELP_MSG = """<b>How to use the bot??</b>
+<i>    Sorry you can't use me 😔,for movies join @trolldcompany
+</i>
+
+<b>© @Trolldcompany</b>"""
+
+
+    ABOUT_MSG = """⭕️<b>My Name : D C Filter Bot</b>
+    
+⭕️<b>Creater :</b> @n_a_c_bot_developers
+
+⭕️<b>Language :</b> <code>Python3</code>
+
+⭕️<b>Library :</b> <a href='https://docs.pyrogram.org/'>Pyrogram 1.0.7</a> 
+
+⭕️<b>support group :</b> <a href='https://t.me/botsfor_you_bot'>click here</a> 
+
+"""
